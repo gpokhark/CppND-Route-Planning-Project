9c27cb3aa4f3f2e66e5e80b8f77c162d34a278f8
gpokhark <gpokhark@gmail.com>
2019-11-10 12:18:12 -0500

    Implement TODO 6 ConstructFinalPath function.

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -82,6 +82,13 @@ std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node 
     std::vector<RouteModel::Node> path_found;
 
     // TODO: Implement your solution here.
+    while (current_node->parent != nullptr)
+    {
+        path_found.push_back(*current_node);
+        distance += current_node->distance(*(current_node->parent));
+        current_node = current_node->parent;
+    }
+    path_found.push_back(*current_node);
 
     distance *= m_Model.MetricScale(); // Multiply the distance by the scale of the map to get meters.
     return path_found;
