cd83165d1537f15c16908a26bf6b15eb2697b516
gpokhark <gpokhark@gmail.com>
2019-11-09 19:50:22 -0500

    Updated main.cpp to accept 4 values for start x,y and end x,y.

--- src/main.cpp
+++ src/main.cpp
@@ -58,12 +58,19 @@ int main(int argc, const char **argv)
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
+    float start_x, start_y, end_x, end_y = 0;
+    std::cout << "The map starts at (0,0) in the left bottom and ends at (100,100) in the top right. \n";
+    std::cout << "Please enter the value for start_x and start_y(Range 0 to 100). \n";
+    std::cin >> start_x >> start_y;
+    std::cout << "Please enter the value for end_x and end_y(Range 0 to 100). \n";
+    std::cin >> end_x >> end_y;
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    // RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
